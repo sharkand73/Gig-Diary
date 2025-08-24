6d3959a6b8c7768684fb0360882186f7f6eb4629
Andrew Sharkey <andrewsharkey@Andrews-MacBook-Pro-2.fritz.box>
2025-08-24 11:08:04 +0100

    Set up react app with router pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import New from './pages/New';
+import List from './pages/List';
+import Edit from './pages/Edit';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route index element={<New />} />
+          <Route path="new" element={<New />} />
+          <Route path="list" element={<List />} />
+          <Route path="edit" element={<Edit />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

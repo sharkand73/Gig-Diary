23465c79b03c43d432a71bd3feacb9601587b754
Andrew Sharkey <andrewsharkey@Andrews-MacBook-Pro-2.fritz.box>
2025-08-24 15:07:33 +0100

    Install Bootstrap

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 function New() {
 
     return (
-        <div>
-            New
+        <div className='container'>
+            <h1>New</h1>
+            <p>
+        Lorem ipsum dolor sit amet. Est voluptatem sapiente ut libero ipsam ut provident perferendis non sunt totam eos maxime numquam aut inventore deleniti in blanditiis magnam. Eos odio dolorem vel voluptatem mollitia est velit nesciunt.
+  
+  Aut eligendi maiores eum fugiat ullam quo consequuntur necessitatibus. Sit consequatur animi ut rerum commodi et nulla quam qui consequatur repudiandae hic doloribus maiores eum animi quisquam et maxime tempora. Eos eligendi sint et quisquam fugit et quas consectetur 33 placeat deleniti et dolorem veniam in perferendis vitae.
+  
+  Qui eligendi ipsum qui necessitatibus suscipit qui molestiae minus? Eos commodi internos et beatae nihil a esse minus est excepturi odio ut voluptatem facilis et aliquam suscipit. A magni tenetur et impedit rerum quo omnis nesciunt qui voluptas quod ea quibusdam nulla aut explicabo ipsa. At praesentium galisum sit optio illum ut incidunt dolor eos tempora laboriosam sed facere sapiente est tenetur facere non minima quasi.
+        </p>
         </div>
     )
 }

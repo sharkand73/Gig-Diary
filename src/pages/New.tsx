0228c504f12ae0cb8e58b3bf9c786423b3d8d5e0
Andrew Sharkey <andrewsharkey@Andrews-MBP-2.fritz.box>
2025-08-26 00:39:25 +0100

    Create new entry form

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,17 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function New() {
 
+    const [formData, setFormData] = useState({
+        act: '',
+        fee: 200,
+        leaveDate: new Date().toISOString().slice(0, 16),
+        returnDate: new Date().toISOString().slice(0, 16),        
+        description: '',
+        venue: '',
+        postcode: '',
+        instrument: 'Upright',
+        calendarSync: false
+    });
+
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        console.log(formData);
+    }
+
+    function onTextChange(e: any) {
+        setFormData({...formData, [e.target.id]: e.target.value});
+    }
+
+    function onCheckboxChange(e: any) {
+        setFormData({...formData, calendarSync: e.target.checked});
+    }
+
+
     return (
-        <div className='container'>
-            <h1>New</h1>
-            <p>
-        Lorem ipsum dolor sit amet. Est voluptatem sapiente ut libero ipsam ut provident perferendis non sunt totam eos maxime numquam aut inventore deleniti in blanditiis magnam. Eos odio dolorem vel voluptatem mollitia est velit nesciunt.
-  
-  Aut eligendi maiores eum fugiat ullam quo consequuntur necessitatibus. Sit consequatur animi ut rerum commodi et nulla quam qui consequatur repudiandae hic doloribus maiores eum animi quisquam et maxime tempora. Eos eligendi sint et quisquam fugit et quas consectetur 33 placeat deleniti et dolorem veniam in perferendis vitae.
-  
-  Qui eligendi ipsum qui necessitatibus suscipit qui molestiae minus? Eos commodi internos et beatae nihil a esse minus est excepturi odio ut voluptatem facilis et aliquam suscipit. A magni tenetur et impedit rerum quo omnis nesciunt qui voluptas quod ea quibusdam nulla aut explicabo ipsa. At praesentium galisum sit optio illum ut incidunt dolor eos tempora laboriosam sed facere sapiente est tenetur facere non minima quasi.
-        </p>
+        <div className='container mt-5 bg-light border-primary'>
+            <div className='card shadow'>
+                <div className='card-body bg-light'>
+
+                    <h1 className='mb-5'>Enter Gig Details</h1>
+                    
+                    <form onSubmit={handleSubmit}>
+                        <div className='mb-4'>
+                            <div className='row'>
+                                <div className='col'>
+                                    <label htmlFor='act' className='form-label'>Act</label>
+                                    <input type='text' className='form-control' id='act' value={formData.act} onChange={onTextChange} required />
+                                </div>
+                                <div className='col'>
+                                    <label htmlFor='fee' className='form-label'>Fee</label>
+                                    <input type='number' className='form-control' id='fee' min='0' max='2000' step='10' value={formData.fee} onChange={onTextChange} required />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='mb-4'>
+                            <div className='row'>
+                                <div className='col'>
+                                    <label htmlFor='leaveDate' className='form-label'>Leave</label>
+                                    <input type='datetime-local' className='form-control' id='leaveDate' value={formData.leaveDate} onChange={onTextChange} />
+                                </div>
+                                <div className='col'>
+                                    <label htmlFor='returnDate' className='form-label'>Return</label>
+                                    <input type='datetime-local' className='form-control' id='returnDate' value={formData.returnDate} onChange={onTextChange} />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='mb-4'>
+                            <div className='row'>
+                                <div className='col'>
+                                    <label htmlFor='venue' className='form-label'>Venue</label>
+                                    <input type='text' className='form-control' id='venue' value={formData.venue} onChange={onTextChange} required />
+                                </div>
+                                <div className='col'>
+                                    <label htmlFor='postcode' className='form-label'>Postcode</label>
+                                    <input type='text' className='form-control' id='postcode' value={formData.postcode} onChange={onTextChange} />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='mb-4'>
+                            <label htmlFor='description' className='form-label'>Description</label>
+                            <textarea className='form-control' id='description' rows={6} value={formData.description} onChange={onTextChange} />
+                        </div>
+
+                        <div className='mb-4'>
+                            <div className='row'>
+                                <div className='col-9'>
+                                    <label htmlFor='instrument' className='form-label'>Instrument</label>
+                                    <select className='form-control' id='instrument' value={formData.instrument} onChange={onTextChange}>
+                                        <option value='Upright'>Upright</option>
+                                        <option value='Electric'>Electric</option>
+                                    </select>
+                                </div>
+                                <div className='col-3 d-flex align-items-end'>
+                                    <div className='form-check form-switch'>
+                                        <input className='form-check-input' type='checkbox' id='calendarSync' checked={formData.calendarSync} onChange={onCheckboxChange} />
+                                        <label className='form-check-label' htmlFor='calendarSync'>Calendar Sync</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <button type="submit" className="btn btn-primary mt-4">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }

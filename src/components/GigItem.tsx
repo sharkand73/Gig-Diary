ea6a01619d827f189a8b55855ca3ef9044fa421f
Andrew Sharkey <andrewsharkey@Andrews-MacBook-Pro-2.local>
2025-09-03 21:52:10 +0100

    Add exclamation mark to incomplete gigItems

--- src/components/GigItem.tsx
+++ src/components/GigItem.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Gig } from '../models/Gig';
 import { formatDate } from '../utilities/common';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExclamation } from '@fortawesome/free-solid-svg-icons';
 
 interface GigItemProps {
     gig: Gig;
@@ -18,13 +20,22 @@ function GigItem({gig}: GigItemProps) {
                             <small className='text-muted'>Date</small>
                             <div className='fw-bold'>{formatDate(gig.leaveDate)}</div>
                         </div>
-                        <div className='col-4'>
+                        <div className='col-3'>
                             <small className='text-muted'>Act</small>
                             <div className='fw-bold'>{gig.act}</div>
                         </div>
                         <div className='col-3'>
                             <small className='text-muted'>Venue</small>
                             <div>{gig.venue}</div>
+                        </div>
+                        <div className='col-1 text-end'>
+                            <span>
+                                {
+                                    !gig.isComplete ? <span className='text-danger'>
+                                        <FontAwesomeIcon icon={faExclamation} size="lg"/>
+                                    </span> : null
+                                }
+                            </span>
                         </div>
                         <div className='col-2 text-end'>
                             <span className='badge bg-primary'>£{gig.fee}</span>

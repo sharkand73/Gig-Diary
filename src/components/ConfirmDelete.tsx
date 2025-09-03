cf7a9cff0ad7add960189dd4de0cdbcd23460aab
Andrew Sharkey <andrewsharkey@Andrews-MacBook-Pro-2.local>
2025-09-03 22:02:30 +0100

    Move confirm delete modal to its own component

---
+++ src/components/ConfirmDelete.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+interface Props {
+    setDeleting: (value: boolean) => void
+    onDelete: () => void
+}
+
+function ConfirmDelete({setDeleting, onDelete}: Props) {
+
+    return (
+        (
+            <div className="modal show d-block" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Confirm Delete</h5>
+                        </div>
+                        <div className="modal-body">
+                            <p>Are you sure you want to delete this gig? This action cannot be undone.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" onClick={() => setDeleting(false)}>
+                                Cancel
+                            </button>
+                            <button type="button" className="btn btn-danger" onClick={() => {
+                                onDelete();
+                            }}>
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    ));
+}
+
+export default ConfirmDelete
--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -4,6 +4,7 @@ import ServiceContainer from '../services/ServiceContainer';
 import { Gig } from '../models/Gig';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faCancel, faList, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons';
+import ConfirmDelete from '../components/ConfirmDelete';
 
 function Edit() {
     const { id } = useParams();
@@ -272,30 +273,7 @@ function Edit() {
                     </div>
                 }
             </div>
-            {deleting && (
-                <div className="modal show d-block" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>
-                    <div className="modal-dialog modal-dialog-centered">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">Confirm Delete</h5>
-                            </div>
-                            <div className="modal-body">
-                                <p>Are you sure you want to delete this gig? This action cannot be undone.</p>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" onClick={() => setDeleting(false)}>
-                                    Cancel
-                                </button>
-                                <button type="button" className="btn btn-danger" onClick={() => {
-                                    onDelete();
-                                }}>
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )}
+            {deleting && <ConfirmDelete setDeleting={setDeleting} onDelete={onDelete}/>}
 
         </div>
     );

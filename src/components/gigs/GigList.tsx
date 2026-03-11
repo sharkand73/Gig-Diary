101666ad2b46dc59e1e3a5c60724acadd59835da
Andrew Sharkey <andrewsharkey@Andrews-MacBook-Pro-2.local>
2026-03-11 19:32:01 +0000

    Make list scroll to next gig

--- src/components/gigs/GigList.tsx
+++ src/components/gigs/GigList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Gig } from '../../models/Gig'
 import GigItem from './GigItem'
 
@@ -8,10 +8,27 @@ interface Props {
 
 function GigList(props: Props) {
     const { gigs } = props
+    const nextGigRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        if (nextGigRef.current) {
+            nextGigRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center'
+            })
+        }
+    }, [gigs])
 
     return (
         <div className='h-100 overflow-auto px-3'>
-            {gigs.map(g => <GigItem gig={g} key={g.id} />)}
+            {gigs.map(g => (
+                <div
+                    key={g.id}
+                    ref={g.isNextGig ? nextGigRef : null}
+                >
+                    <GigItem gig={g} />
+                </div>
+            ))}
         </div>
     )
 }
